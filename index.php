a0edc781fb561ecf48aed35b54ba293a24aa1257
Tomas Stankevicius <s.tomas@outlook.com>
2019-07-21 17:01:44 +0300

    added some more text

--- index.php
+++ index.php
@@ -90,6 +90,8 @@
                 <p>This webpage was built as a homework assignment. These were the steps to build it:</p>
                 <ul>
                     <li>All the code lives in this repo: <a href="https://github.com/Koperis/homework">Github</a></li>
+                    <li>Also, same code lives in my personal git repo in Azure DevOps services</li>
+                    <li>When I do a code change it gets pushed to both repos and Azure DevOps CI pipeline gets kicked off</li>
                     <li>A dedicated server (a build machine in azure cloud) fetches the code (ansible playbooks and the index.php)</li>
                     <li>Ansible builds a dedicated server which is used to host this webpage. The server is built in azure cloud. Server OS is CentOS linux.</li>
                     <li>Ansible installs nginx and redis database and all their dependencies.</li>
